4e90477f9fbf54bdecc968d3be782543fc8b5ea4
The-Deepak-Pandey <deepak.pandey@students.iiit.ac.in>
2025-07-05 14:33:49 +0530

    db connection succeeded

--- connect.py
+++ connect.py
@@ -1,13 +1,15 @@
 import mysql.connector
 import os
+from dotenv import load_dotenv
 
+load_dotenv()  # Load environment variables from .env file
 
 # replace with your actual database info
 conn = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password=os.getenv("SQL_PASSWORD"),
-    database="library"
+    host=os.getenv("DB_HOST"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"), 
+    database=os.getenv("DB_NAME")
 )
 
 print("✅ Connected to database!")
